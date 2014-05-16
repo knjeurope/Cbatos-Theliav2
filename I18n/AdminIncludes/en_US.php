c134ca0d2418313eb9c8d202e7e74351837cc269
knjeurope <lespesjulien33@gmail.com>
2014-05-16 18:24:16 +0200

    Fixed issue #1

---
+++ I18n/AdminIncludes/en_US.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+    'Configuration de Atos' => 'Atos Setup',
+    'EUR' => 'EUR',
+    'Immediate charge' => 'Immediate charge',
+    'NON' => 'NO',
+    'OUI' => 'YES',
+    'Please change the access rights' => 'Please change the access rights',
+    'Save' => 'Save',
+    'USD' => 'USD',
+    'Url return automatic Atos' => 'Url return automatic Atos',
+    'Url return back Atos' => 'Url return back Atos',
+    'Your Merchant Id' => 'Your Merchant Id',
+    'Your Path File Bin Request and Responses' => 'Your Path File Bin Request and Responses',
+    'day' => 'day',
+    'days' => 'days',
+);
